d3f1cc9db35a3bbd64ab527e427640cba6a8581b
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-05 14:46:04 -0600

    Update BeerOrderBoostrap

--- src/main/java/org/beer/works/beer/order/boostrap/BeerOrderBoostrap.java
+++ src/main/java/org/beer/works/beer/order/boostrap/BeerOrderBoostrap.java
@@ -1,7 +1,11 @@
 package org.beer.works.beer.order.boostrap;
 
 import lombok.RequiredArgsConstructor;
+import org.beer.works.beer.order.domain.Customer;
+import org.beer.works.beer.order.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 public class BeerOrderBoostrap implements CommandLineRunner {
@@ -11,11 +15,19 @@ public class BeerOrderBoostrap implements CommandLineRunner {
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
 
+    private final CustomerRepository customerRepository;
+
     @Override
     public void run(String... args) throws Exception {
         loadCustomerData();
     }
 
     private void loadCustomerData() {
+        if(customerRepository.count() == 0){
+            customerRepository.save(Customer.builder()
+                    .customerName(TASTING_ROOM)
+                    .apiKey(UUID.randomUUID())
+                    .build());
+        }
     }
 }
