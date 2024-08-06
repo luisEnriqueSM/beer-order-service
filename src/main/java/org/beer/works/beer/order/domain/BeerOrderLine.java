d03de2669483b910418d8be14ff538f06f70728e
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-08-06 17:20:10 -0600

    Add upc attribute to BeerOrderLine entity

--- src/main/java/org/beer/works/beer/order/domain/BeerOrderLine.java
+++ src/main/java/org/beer/works/beer/order/domain/BeerOrderLine.java
@@ -22,14 +22,16 @@ public class BeerOrderLine extends BaseEntity{
     private UUID beerId;
     private Integer orderQuantity = 0;
     private Integer quantityAllocated = 0;
+    private String upc;
 
     @Builder
     public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate,
-                         BeerOrder beerOrder, UUID beerId, Integer orderQuantity,
+                         BeerOrder beerOrder, UUID beerId, String upc, Integer orderQuantity,
                          Integer quantityAllocated){
         super(id, version, createdDate, lastModifiedDate);
         this.beerOrder = beerOrder;
         this.beerId = beerId;
+        this.upc = upc;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
     }
